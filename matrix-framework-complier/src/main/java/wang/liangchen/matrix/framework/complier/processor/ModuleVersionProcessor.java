12082968f0fa7bb5596b071beaffac959b2b1bcd
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2023-01-09 18:19:22 +0800

    remove unsafe

--- matrix-framework-complier/src/main/java/wang/liangchen/matrix/framework/complier/processor/ModuleVersionProcessor.java
+++ matrix-framework-complier/src/main/java/wang/liangchen/matrix/framework/complier/processor/ModuleVersionProcessor.java
@@ -7,14 +7,12 @@ import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.util.Context;
-import sun.misc.Unsafe;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
-import java.lang.reflect.Field;
 import java.util.Set;
 
 /**
@@ -30,17 +28,6 @@ public class ModuleVersionProcessor extends AbstractProcessor {
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
-        try {
-            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-            theUnsafe.setAccessible(true);
-            Unsafe u = (Unsafe) theUnsafe.get(null);
-
-            Class<?> cls = Class.forName("jdk.internal.module.IllegalAccessLogger");
-            Field logger = cls.getDeclaredField("logger");
-            u.putObjectVolatile(cls, u.staticFieldOffset(logger), null);
-        } catch (Throwable t) {
-            // We shall ignore it; the effect of this code failing is that the user gets to see a warning they remove with various --add-opens magic.
-        }
         super.init(processingEnv);
         this.processingEnv = processingEnv;
         this.javacTrees = JavacTrees.instance(processingEnv);
