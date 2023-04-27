aae4ef15a2d024ced438dfad1a17c7565876c704
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2023-04-27 16:16:23 +0800

    optimize and upgrade springboot 3.0.6

--- matrix-framework-data/src/main/java/wang/liangchen/matrix/framework/data/configuration/CachedStandaloneDaoConfiguration.java
+++ matrix-framework-data/src/main/java/wang/liangchen/matrix/framework/data/configuration/CachedStandaloneDaoConfiguration.java
@@ -3,17 +3,17 @@ package wang.liangchen.matrix.framework.data.configuration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.lang.Nullable;
-import wang.liangchen.matrix.cache.sdk.cache.CacheManager;
+import wang.liangchen.matrix.cache.sdk.cache.MatrixCacheManager;
 import wang.liangchen.matrix.framework.data.cache.CacheOperator;
 import wang.liangchen.matrix.framework.data.dao.StandaloneDao;
 
 /**
  * @author Liangchen.Wang 2022-10-02 12:06
  */
-@ConditionalOnClass(name = {"wang.liangchen.matrix.cache.sdk.cache.CacheManager"})
+@ConditionalOnClass(name = {"wang.liangchen.matrix.cache.sdk.cache.MatrixCacheManager"})
 public class CachedStandaloneDaoConfiguration {
     @Bean
-    public StandaloneDao cachedStandaloneDao(@Nullable CacheManager cacheManager) {
-        return new StandaloneDao(new CacheOperator(cacheManager));
+    public StandaloneDao cachedStandaloneDao(@Nullable MatrixCacheManager matrixCacheManager) {
+        return new StandaloneDao(new CacheOperator(matrixCacheManager));
     }
 }
