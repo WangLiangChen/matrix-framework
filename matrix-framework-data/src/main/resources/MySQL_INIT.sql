d36dc6da4625273fb6ed8a02bb586534280526b9
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2024-03-31 20:23:25 +0800

    remove unused sql

--- matrix-framework-data/src/main/resources/MySQL_INIT.sql
+++ matrix-framework-data/src/main/resources/MySQL_INIT.sql
@@ -5,24 +5,3 @@ create table if not exists matrix_sequence
     sequence_number bigint      not null comment '当前序号',
     primary key(sequence_group,sequence_key)
 ) comment '序列生成器';
-
-/*==============================================================*/
-/* Table: matrix_columns                                        */
-/*==============================================================*/
-create table matrix_columns
-(
-    column_id            bigint not null  comment 'PrimaryKey',
-    column_key           varchar(36) not null  comment 'UniqueKey',
-    column_group           varchar(36) not null  comment '分组标识,用于数据隔离',
-    table_name           varchar(36) not null  comment '表名',
-    column_name          varchar(36) not null  comment '列名',
-    data_type            varchar(36) not null  comment '值类型',
-    column_default       varchar(100) not null  comment '默认值',
-    is_nullable          tinyint not null  comment 'can be null',
-    column_regex         varchar(100) not null  comment '值校验正则',
-    column_comment       varchar(100) not null  comment '列说明',
-    primary key (column_id),
-    unique key ak_unique_key (column_key)
-);
-
-alter table matrix_columns comment '扩展列';
