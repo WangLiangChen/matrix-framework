a7b3cd25b6dd29912a9685caa18eb74a11c21783
Liangchen.Wang <sintrue@hotmail.com>
2025-07-24 15:02:29 +0800

    upgrade springboot optimize module name

---
+++ matrix-framework-commons/src/main/java/wang/liangchen/matrix/framework/commons/AstronomicalAlgorithmUtil.java
@@ -0,0 +1,11 @@
+package wang.liangchen.matrix.framework.commons;
+
+import java.time.ZonedDateTime;
+
+public enum AstronomicalAlgorithmUtil {
+    INSTANCE;
+
+    public void meanSolarTime(ZonedDateTime zonedDateTime){
+
+    }
+}
--- matrix-framework-samples/src/test/java/com/sintrue/samples/test/MainTest.java
+++ matrix-framework-samples/src/test/java/com/sintrue/samples/test/MainTest.java
@@ -4,11 +4,15 @@ import com.sintrue.samples.api.SampleRequest;
 import org.junit.jupiter.api.Test;
 import wang.liangchen.matrix.framework.commons.jackson.JacksonUtil;
 import wang.liangchen.matrix.framework.commons.object.ObjectUtil;
+import wang.liangchen.matrix.framework.data.enumeration.DataMode;
 import wang.liangchen.matrix.framework.data.pagination.OrderBy;
 import wang.liangchen.matrix.framework.data.pagination.Pagination;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 public class MainTest {
@@ -67,5 +71,41 @@ public class MainTest {
             }
             System.out.println(stringBuilder.toString());
         }
+    }
+
+    @Test
+    public void testMove() {
+    }
+
+    @Test
+    public void testMeanSolarTime() {
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(2025, 3, 21, 15, 18, 0, 0, ZoneId.systemDefault());
+        long eot = equationOfTime(zonedDateTime);
+        zonedDateTime = meanSolarTime(zonedDateTime,116.46);
+        System.out.println(zonedDateTime);
+
+        System.out.println(zonedDateTime.plusSeconds(eot));
+    }
+
+    public ZonedDateTime meanSolarTime(ZonedDateTime zonedDateTime, double localLongitude) {
+        ZoneOffset offset = zonedDateTime.getOffset();
+        int offsetHours = offset.getTotalSeconds() / 3600;
+        int centralLongitudeOfZone = offsetHours * 360 / 24;
+        double deltaSeconds = (localLongitude - centralLongitudeOfZone) * 3600 / 15;
+        return zonedDateTime.plusSeconds(Math.round(deltaSeconds));
+    }
+
+
+    public long equationOfTime(ZonedDateTime localDateTime) {
+        ZonedDateTime utc = localDateTime.withZoneSameInstant(ZoneOffset.UTC);
+        int year = utc.getYear();
+        int doy = utc.getDayOfYear();
+        // 计算地球在其轨道上的平近点角(Mean Anomaly)
+        double d = 6.24004077 + 0.01720197 * (365.2425 * (year - 2000) + doy);
+        // 地轴的倾斜(Obliquity)
+        double obliquity = 9.863 * Math.sin(2 * d + 3.5932);
+        // 地球轨道的偏心率(Eccentricity)
+        double eccentricity = -7.659 * Math.sin(d);
+        return Math.round((obliquity + eccentricity)*60);
     }
 }
